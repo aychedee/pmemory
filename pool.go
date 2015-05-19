1619623ad2f5862fb8f286d37c6743a5cee6ae6f
Hansel Dunlop <hansel@interpretthis.org>
2015-05-19 10:44:33 +0100

    Add a basic pool for redis connections

---
+++ pool.go
@@ -0,0 +1,24 @@
+package pmemory
+
+import (
+	"github.com/garyburd/redigo/redis"
+	"time"
+)
+
+func New() (c *redis.Pool) {
+	return &redis.Pool{
+		MaxIdle:     3,
+		IdleTimeout: 240 * time.Second,
+		Dial: func() (redis.Conn, error) {
+			c, err := redis.Dial("tcp", ":6379")
+			if err != nil {
+				return nil, err
+			}
+			return c, err
+		},
+		TestOnBorrow: func(c redis.Conn, t time.Time) error {
+			_, err := c.Do("PING")
+			return err
+		},
+	}
+}
