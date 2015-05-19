82d6dcab546bf5dbaf2a2d70f261f04d88c15050
Hansel Dunlop <hansel@interpretthis.org>
2015-05-19 13:01:50 +0100

    Test backend for pmemory implemented

--- pool.go
+++ pool.go
@@ -1,3 +1,10 @@
+// Copyright 2015 Hansel Dunlop
+// See LICENSE for licensing information
+
+// This package contains a Redis connection pool, and using an interface that
+// has a switchable backend. At the moment the only other backend is TestBackend
+// which lets you use this pool in a unit test environment
+
 package pmemory
 
 import (
@@ -5,8 +12,33 @@ import (
 	"time"
 )
 
-func New() (c *redis.Pool) {
-	return &redis.Pool{
+type Pool struct {
+	back Backend
+}
+
+func (p *Pool) Do(cmd string, args ...interface{}) (r interface{}, e error) {
+	return p.back.Do(cmd, args...)
+}
+
+type Backend interface {
+	setup()
+	Do(cmd string, args ...interface{}) (interface{}, error)
+}
+
+type RedisBackend struct {
+	pool *redis.Pool
+	conn redis.Conn
+}
+
+func (b *RedisBackend) Do(cmd string, args ...interface{}) (r interface{}, e error) {
+	b.conn = b.pool.Get()
+	defer b.conn.Close()
+	r, e = b.conn.Do(cmd, args...)
+	return
+}
+
+func (b *RedisBackend) setup() {
+	b.pool = &redis.Pool{
 		MaxIdle:     3,
 		IdleTimeout: 240 * time.Second,
 		Dial: func() (redis.Conn, error) {
@@ -21,4 +53,17 @@ func New() (c *redis.Pool) {
 			return err
 		},
 	}
+}
+
+type TestBackend struct{}
+
+func (b *TestBackend) Do(cmd string, args ...interface{}) (r interface{}, e error) {
+	return nil, nil
+}
+
+func (b *TestBackend) setup() {}
+
+func New(b Backend) (c *Pool) {
+	b.setup()
+	return &Pool{back: b}
 }
