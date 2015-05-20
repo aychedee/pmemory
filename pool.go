bfe483e57ccad54f78de2e7f38c6000c497bfb4a
Hansel Dunlop <hansel@interpretthis.org>
2015-05-20 16:26:39 +0100

    Handle commands that need to use a connection multiple times

--- pool.go
+++ pool.go
@@ -20,9 +20,19 @@ func (p *Pool) Do(cmd string, args ...interface{}) (r interface{}, e error) {
 	return p.back.Do(cmd, args...)
 }
 
+func (p *Pool) Send(cmd string, args ...interface{}) (e error) {
+	return p.back.Send(cmd, args...)
+}
+
+func (p *Pool) Close() {
+	p.back.Close()
+}
+
 type Backend interface {
 	setup()
 	Do(cmd string, args ...interface{}) (interface{}, error)
+	Send(cmd string, args ...interface{}) error
+	Close()
 }
 
 type RedisBackend struct {
@@ -31,10 +41,24 @@ type RedisBackend struct {
 }
 
 func (b *RedisBackend) Do(cmd string, args ...interface{}) (r interface{}, e error) {
-	b.conn = b.pool.Get()
-	defer b.conn.Close()
+	if b.conn == nil {
+		b.conn = b.pool.Get()
+	}
 	r, e = b.conn.Do(cmd, args...)
 	return
+}
+
+func (b *RedisBackend) Send(cmd string, args ...interface{}) (e error) {
+	if b.conn == nil {
+		b.conn = b.pool.Get()
+	}
+	e = b.conn.Send(cmd, args...)
+	return
+}
+
+func (b *RedisBackend) Close() {
+	b.conn.Close()
+	b.conn = nil
 }
 
 func (b *RedisBackend) setup() {
@@ -58,8 +82,21 @@ func (b *RedisBackend) setup() {
 type TestBackend struct{}
 
 func (b *TestBackend) Do(cmd string, args ...interface{}) (r interface{}, e error) {
+	switch {
+	case cmd == "SMEMBERS":
+		return [][]byte{[]byte("12"), []byte("234"), []byte("2")}, nil
+	case cmd == "EXEC":
+		return []map[string]string{map[string]string{"dear": "me"}}, nil
+	}
 	return nil, nil
 }
+
+func (p *TestBackend) Send(cmd string, args ...interface{}) (e error) {
+
+	return nil
+}
+
+func (b *TestBackend) Close() {}
 
 func (b *TestBackend) setup() {}
 
